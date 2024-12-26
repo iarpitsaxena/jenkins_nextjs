3ae520e3c79e06fe35be523ba08de8813d3f0b80
ditinagrawal <ditin.agrawal05@gmail.com>
2024-12-26 11:37:10 +0530

    Feebo

--- app/b/layout.tsx
+++ app/b/layout.tsx
@@ -2,7 +2,7 @@ export default function BoardLayout(
     { children }: { children: React.ReactNode }
 ) {
     return (
-        <div className="bg-base-200 p-8">
+        <div className="bg-base-200 p-8 min-h-screen">
             {children}
         </div>
     );
